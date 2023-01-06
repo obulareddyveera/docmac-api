5c05b04dc35d51cd703b2d1f6cb4724792ba5f02
obulareddyveera <obulareddy.veera@gmail.com>
2023-01-07 01:32:02 +0530

    create, update employee detials completed

---
+++ prisma/migrations/20230106045616_init/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Person" ADD COLUMN     "doa" STRING;
+ALTER TABLE "Person" ADD COLUMN     "doj" STRING;
+ALTER TABLE "Person" ADD COLUMN     "referal" STRING;
+ALTER TABLE "Person" ADD COLUMN     "status" STRING;
